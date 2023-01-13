361841902f7b227620bdeaa9575da166bf2f6405
damiansoch <66294165+damiansoch@users.noreply.github.com>
2023-01-13 09:27:08 +0000

    collors added to console

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+  mongoose.set('strictQuery', false);
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
 
-    console.log(`MongoDB Connected:${conn.connection.host}`);
+    console.log(`MongoDB Connected:${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.log(`Error: ${error.message}`);
+    console.log(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
