fd7156d44d8e70390d2f62247a611ad8f7f7c8f2
damiansoch <66294165+damiansoch@users.noreply.github.com>
2023-01-13 13:25:48 +0000

    custom error handling completed

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMidleware.js';
 
 dotenv.config();
 
@@ -15,14 +17,13 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+//custom error handling routes not found
+app.use(notFound);
+
+//custom error handling for error
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
