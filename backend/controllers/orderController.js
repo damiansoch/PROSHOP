0e0da41440aae8f0abef9e2e086215a05f243639
damiansoch <66294165+damiansoch@users.noreply.github.com>
2023-01-20 16:06:10 +0000

    update order to paid backend done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,3 +54,29 @@ export const getOrderById = asyncHandler(async (req, res) => {
     throw new Error('Order notr found');
   }
 });
+
+// @desc    Update order to paid
+// @route   get /api/orders/:id/pay
+// @access  Private
+export const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      //this is going to come from payPal
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order notr found');
+  }
+});
