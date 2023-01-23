1c9884f687dfb9bfcc8a015ea600286793881eb8
damiansoch <66294165+damiansoch@users.noreply.github.com>
2023-01-23 14:43:46 +0000

    getAllOrdersAdmin back completed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -89,3 +89,12 @@ export const getMyOrders = asyncHandler(async (req, res) => {
 
   res.json(orders);
 });
+
+// @desc    Get all orders
+// @route   get /api/orders
+// @access  Private/Admin
+export const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name');
+
+  res.json(orders);
+});
