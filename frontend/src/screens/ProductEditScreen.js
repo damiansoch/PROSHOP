fe10fea95159b6e6a6e70088e8f552da96d91d7f
damiansoch <66294165+damiansoch@users.noreply.github.com>
2023-01-23 11:00:14 +0000

    create product front completed

---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,142 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+
+import { useParams, useNavigate } from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import FormContainer from '../components/FormContainer';
+
+import { listProductDetails } from '../actions/productActions';
+
+const ProductEditScreen = () => {
+  const navigate = useNavigate();
+
+  const { id: productId } = useParams();
+
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [brand, setBrand] = useState('');
+  const [category, setCategory] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState(0);
+  const [countInStock, setCountInStock] = useState(0);
+
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  //
+  useEffect(() => {
+    if (!product.name || product._id !== productId) {
+      dispatch(listProductDetails(productId));
+    } else {
+      setName(product.name);
+      setImage(product.image);
+      setBrand(product.brand);
+      setCategory(product.category);
+      setDescription(product.description);
+      setPrice(product.price);
+      setCountInStock(product.countInStock);
+    }
+  }, [product, dispatch, productId, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //update product
+  };
+
+  return (
+    <>
+      <Link to={'/admin/productlist'} className='btn btn-light my-3'>
+        Go back
+      </Link>
+      <FormContainer>
+        <h1>Edit product</h1>
+
+        {/* {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
+        {loadingUpdate && <Loader />} */}
+
+        {error && <Message variant='danger'>{error}</Message>}
+        {loading && <Loader />}
+
+        <Form onSubmit={submitHandler}>
+          <Form.Group className='my-2' controlId='name'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter your name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className='my-2' controlId='price'>
+            <Form.Label>Price</Form.Label>
+            <Form.Control
+              type='number'
+              placeholder='Enter price €'
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className='my-2' controlId='image'>
+            <Form.Label>Image</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter image url'
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className='my-2' controlId='brand'>
+            <Form.Label>Brand</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter brand'
+              value={brand}
+              onChange={(e) => setBrand(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className='my-2' controlId='category'>
+            <Form.Label>Category</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter category'
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className='my-2' controlId='counInStock'>
+            <Form.Label>Count in stoch</Form.Label>
+            <Form.Control
+              type='number'
+              placeholder='Enter count in stock'
+              value={countInStock}
+              onChange={(e) => setCountInStock(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className='my-2' controlId='description'>
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              as='textarea'
+              type='text'
+              placeholder='Enter description'
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button className='my-2' type='submit' variant='primary'>
+            Update
+          </Button>
+        </Form>
+      </FormContainer>
+    </>
+  );
+};
+
+export default ProductEditScreen;
