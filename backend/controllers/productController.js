21a4af8733c368d8a32091c17b970f41e5d2b3d9
damiansoch <66294165+damiansoch@users.noreply.github.com>
2023-01-23 06:41:29 +0000

    delete product back completed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,3 +24,18 @@ export const getProductById = asyncHandler(async (req, res) => {
     throw new Error('Product not found');
   }
 });
+
+// @desc    delete single product
+// @route   DELETE /api/products/:id
+// @access  private/admin
+export const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: `Product ${product.name} removed` });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
